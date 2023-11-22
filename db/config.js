46368373f4937959736a3716ef4eecc7c423afb4
Eduardo ML <edu@eml.run>
2023-11-22 08:23:44 -0600

    Vercel Deploy (free-plan)

--- db/config.js
+++
@@ -1,6 +0,0 @@
-// Archivo de configuración para migrations (dev08)
-
-module.exports = {
-    development: { storage: "./api/database.db", dialect: "sqlite" },
-    production: { storage: "./api/database.db", dialect: "sqlite" },
-};
