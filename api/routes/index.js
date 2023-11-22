34de8c85260ed3b6b41c2b4322886829dc9d2933
Eduardo ML <edu@eml.run>
2023-11-21 19:47:48 -0600

    Deployment to Vercel (Free plan)

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const itemsRouter = require('./items.router')
+// const itemsRouter = require('./items.router')
 const dailiesRouter = require('./dailies.router')
 
 function routerApi(app) {
     const router = express.Router()
     app.use('/api/v1', router)
     
-    router.use('/items', itemsRouter);
+    // router.use('/items', itemsRouter);
     router.use('/dailies', dailiesRouter); // (dev07)
 }
 
